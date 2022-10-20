a3b06b96272d9e369969b8b2b7ae0f1fd4daab17
Supraj Maripeddi <suprajmaripeddi@C02FN3Z4Q6LR.local>
2022-10-20 16:29:29 +0530

    adding s3 block access terraform

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -8,3 +8,8 @@ resource "aws_s3_account_public_access_block" "example" {
   block_public_acls   = true
   block_public_policy = true
 }
+resource "aws_s3_bucket" "sandbox_bucket1" {
+  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}-supraj-testing"
+  acl    = "private"
+}
+
